51526ac4a99bb2a08af8ad13e3bb4221e1111117
ysukkar33 <dev.yz1992@gmail.com>
2026-07-08 16:29:45 +0300

    Add GitHub Actions workflow for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-portfolio/",
+  base: '/my-portfolio/', // IMPORTANT: Must match your repo name
 })
